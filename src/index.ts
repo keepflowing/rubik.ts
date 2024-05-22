c270ce18f7c3e4bc6cbd8656841042c323d82dda
keepflowing <132106900+keepflowing@users.noreply.github.com>
2024-05-22 17:07:23 -0500

    Add WebGL main

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,37 @@
 import Cube from "./RubiksCube";
 
+// WebGL main
+function main() : void {
+  // Vertex shader program
+  const vsSource = `
+  attribute vec4 aVertexPosition;
+  uniform mat4 uModelViewMatrix;
+  uniform mat4 uProjectionMatrix;
+  void main() {
+    gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+  }
+  `;
+
+  // Fragment shader program
+  const fsSource = `
+    void main() {
+      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+  `;
+
+  const canvas = document.querySelector("canvas");
+  if(!canvas) return;
+
+  const gl = canvas.getContext("webgl");
+  if(!gl) return;
+
+  gl.clearColor(0.3, 0.3, 0.3, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+}
+
+main();
+
 const cube = new Cube();
 cube.print();
 
