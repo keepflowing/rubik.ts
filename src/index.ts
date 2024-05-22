6acb1015868c3b140cedf34c2c16e86066385c07
keepflowing <132106900+keepflowing@users.noreply.github.com>
2024-05-22 16:25:42 -0500

    Add eventlistener to move cube with keyboard input

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,23 @@
 import Cube from "./RubiksCube";
 
 const cube = new Cube();
-
 cube.print();
 
-cube.moveBack(true);
-cube.print();
+document.addEventListener("keydown", (e) => {
+  if(e.key == "a") cube.moveLeft(false);
+  else if(e.key == "A") cube.moveLeft(true);
+  if(e.key == "d") cube.moveRight(false);
+  else if(e.key == "D") cube.moveRight(true);
+  if(e.key == "w") cube.moveUp(false);
+  else if(e.key == "W") cube.moveUp(true);
+  if(e.key == "s") cube.moveDown(false);
+  else if(e.key == "S") cube.moveDown(true);
 
-/*
-cube.moveLeft(true);
-cube.print();
+  if(e.key == "k") cube.moveFront(false);
+  else if(e.key == "K") cube.moveFront(true);
+  if(e.key == "l") cube.moveBack(false);
+  else if(e.key == "L") cube.moveBack(true);
 
-cube.moveLeft(false);
-cube.print();
-
-cube.moveRight(true);
-cube.print();
-*/
+  console.clear();
+  cube.print();
+})
