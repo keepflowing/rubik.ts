65d0fe07b9209c2e5b771e0afda26c0ceabcd063
keepflowing <132106900+keepflowing@users.noreply.github.com>
2024-05-25 16:44:13 -0500

    Calulate and rotate based on relative X and Y axis

--- src/RubiksCube/RubikPiece.ts
+++ src/RubiksCube/RubikPiece.ts
@@ -5,10 +5,10 @@ export class RubikPiece {
   index: number;
   vec: Vec3 = new Vec3();
   transformation = new Mat4();
-  rotated = {
-    x: 0,
-    y: 0,
-    z: 0
+  r = {
+    x: new Vec3(1, 0, 0),
+    y: new Vec3(0, 1, 0),
+    z: new Vec3(0, 0, 1)
   }
 
   constructor(index: number, size: number) {
@@ -18,11 +18,22 @@ export class RubikPiece {
     this.vec.x = topLeft + 2.1*(index%size);
     this.vec.y = -topLeft - 2.1*((Math.floor(index/size)%size));
     this.vec.z = -topLeft - 2.1*(Math.floor(index/(size*size)));
-
-    this.transformation.setTranslate(0.0, 0.0, 0.0);
   }
 
   rotateX(a: number) {
-    // todo
+    const r = this.r.x;
+    /*if(this.r.z%180 == 0) {
+      if(this.r.y%180 == 0) this.transformation.rotate(a, 1.0, 0.0, 0.0);
+      else this.transformation.rotate(a, 0.0, 0.0, 1.0);*/
+    this.transformation.rotate(a, r.x, r.y, r.z);
+  }
+
+  rotateY(a: number) {
+    const r = this.r.y;
+    /*
+    if(this.r.z%180 == 0) {
+      if(this.r.x%180 == 0) this.transformation.rotate(a, 0.0, 1.0, 0.0);
+      else this.transformation.rotate(a, 0.0, 0.0, 1.0);*/
+    this.transformation.rotate(a, r.x, r.y, r.z);
   }
 }
--- src/RubiksCube/index.ts
+++ src/RubiksCube/index.ts
@@ -25,7 +25,13 @@ export class RubiksCube {
     for(let i = column; i < Math.pow(this.size, 3); i+= this.size) {
       this.pieces[i].rotateX(-a);
       this.pieces[i].vec = rot.multiplyVector3(this.pieces[i].vec);
-      this.pieces[i].rotated.x += -a;
+      if(turn+Math.abs(a) == 90) {
+        const rx = this.pieces[i].r.x;
+        const ry = this.pieces[i].r.y;
+        const rz = this.pieces[i].r.z;
+        this.pieces[i].r.y = new Mat4().setRotate(90, rx.x, rx.y, rx.z).multiplyVector3(ry);
+        this.pieces[i].r.z = new Mat4().setRotate(90, rx.x, rx.y, rx.z).multiplyVector3(rz);
+      }
     }
 
     if(turn+Math.abs(a) == 90) {
@@ -51,8 +57,15 @@ export class RubiksCube {
 
     for(let r = column; r < 27; r+=9) {
       for(let i = 0; i < 3; i++) {
+        this.pieces[r+i].rotateY(-a);
         this.pieces[r+i].vec = rot.multiplyVector3(this.pieces[r+i].vec);
-        this.pieces[i].rotated.y += a;
+        if(turn+Math.abs(a) == 90) {
+          const rx = this.pieces[r+i].r.x;
+          const ry = this.pieces[r+i].r.y;
+          const rz = this.pieces[r+i].r.z;
+          this.pieces[r+i].r.x = new Mat4().setRotate(90, ry.x, ry.y, ry.z).multiplyVector3(rx);
+          this.pieces[r+i].r.z = new Mat4().setRotate(90, ry.x, ry.y, ry.z).multiplyVector3(rz);
+        }
       }
     }
 
